05f09c688042ae84c42578267544594f4b363b86
Sebastian Orozco Peña <seorozc@bancolombia.com.co>
2019-03-30 10:25:51 -0500

    proyectos base paraconsumidor y publicador

---
+++ tripadvisor/src/main/java/devco/tripadvisor/TripadvisorApplication.java
@@ -0,0 +1,13 @@
+package devco.tripadvisor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TripadvisorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TripadvisorApplication.class, args);
+	}
+
+}
